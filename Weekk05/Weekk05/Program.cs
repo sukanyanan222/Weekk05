5ebd9399580b17d63ab442e0fc7f40406de3c4c9
Sukanya <Sukanyak21130@gmail.com>
2017-09-08 15:03:29 +0700

    W05P01

---
+++ Weekk05/Weekk05/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Weekk05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Line 1");
+            Console.WriteLine("Line 2");
+            Console.WriteLine("Line 3");
+        line4:
+            Console.WriteLine("Line 4");
+            Console.WriteLine("Line 5");
+            Console.WriteLine("Line 6");
+            goto line10;
+            Console.WriteLine("Line 7");
+            Console.WriteLine("Line 8");
+            Console.WriteLine("Line 9");
+        line10:
+            Console.WriteLine("Line 10");
+
+        }
+    }
+}
